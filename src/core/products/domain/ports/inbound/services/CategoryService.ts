09cf1645286b569f923592916527ba79e6c596f8
hectorruiz1 <hector.ruiz@kubide.es>
2024-02-29 20:45:08 +0100

    chore(files): removed residual files

--- src/core/products/domain/ports/inbound/services/CategoryService.ts
+++
@@ -1,7 +0,0 @@
-import { Category } from '@core/category/domain/interfaces/Category'
-
-export interface CategoryServicePort {
-    findAll(): Promise<Category[]>
-    findByName(name: string): Promise<Category>
-    findById(id: number): Promise<Category>
-}
